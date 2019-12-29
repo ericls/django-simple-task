b176e5b793f3926c5f55ecc5585ad82a19a35685
Shen Li <dustet@gmail.com>
2019-12-29 11:03:00 -0500

    github actions

--- django_simple_task/__init__.py
+++ django_simple_task/__init__.py
@@ -2,6 +2,6 @@ from .task import defer
 from .middleware import django_simple_task_middlware
 
 __all__ = ["defer", "django_simple_task_middlware"]
-__version__ = '0.1.0'
+__version__ = "0.1.0"
 
 default_app_config = "django_simple_task.apps.DjangoSimpleTaskConfig"
